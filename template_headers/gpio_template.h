a240048fdebd6a65c23710a0c770f26d056d13f6
harrington.234 <harrington.234@osu.edu>
2014-12-02 15:46:16 -0500

    Revised system code to match new MCN v2.0 pinouts

--- template_headers/gpio_template.h
+++ template_headers/gpio_template.h
@@ -8,38 +8,41 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
-#define 	CLEARLED0()				GpioDataRegs.GPACLEAR.bit.GPIO29 = 1
-#define		SETLED0()				GpioDataRegs.GPASET.bit.GPIO29 = 1
-#define 	TOGGLELED0()			GpioDataRegs.GPATOGGLE.bit.GPIO29 = 1
+#define 	CLEARLED0()				GpioDataRegs.GPACLEAR.bit.GPIO22 = 1
+#define		SETLED0()				GpioDataRegs.GPASET.bit.GPIO22 = 1
+#define 	TOGGLELED0()			GpioDataRegs.GPATOGGLE.bit.GPIO22 = 1
 
-#define 	CLEARLED1()				GpioDataRegs.GPACLEAR.bit.GPIO9 = 1
-#define		SETLED1()				GpioDataRegs.GPASET.bit.GPIO9 = 1
-#define 	TOGGLELED1()			GpioDataRegs.GPATOGGLE.bit.GPIO9 = 1
+#define 	CLEARLED1()				GpioDataRegs.GPACLEAR.bit.GPIO23 = 1
+#define		SETLED1()				GpioDataRegs.GPASET.bit.GPIO23 = 1
+#define 	TOGGLELED1()			GpioDataRegs.GPATOGGLE.bit.GPIO23 = 1
 
+/*
 #define 	CLEAR12V()				GpioDataRegs.GPACLEAR.bit.GPIO20 = 1
 #define		SET12V()				GpioDataRegs.GPASET.bit.GPIO20 = 1
 #define 	TOGGLE12V()				GpioDataRegs.GPATOGGLE.bit.GPIO20 = 1
+*/
 
-#define		READGPBUTTON()			GpioDataRegs.GPADAT.bit.GPIO21
+#define		READGPBUTTON()			GpioDataRegs.GPADAT.bit.GPIO11
 
 
 /**
- * Configures the GPIO pin 21 to the GPIO button.
+ * Configures the GPIO pin 11 to the GPIO button.
  */
 void ConfigGPButton();
 
 /**
  * Configures the GPIO pin 20 to control 12V.
+ * NO LONGER USED IN MCN V2.0
  */
-void Config12V();
+//void Config12V();
 
 /**
- * Configures the GPIO pin 9 to control LED1.
+ * Configures the GPIO pin 23 to control LED1.
  */
 void ConfigLED1();
 
 /**
- * Configures the GPIO pin 29 to control LED0.
+ * Configures the GPIO pin 22 to control LED0.
  */
 void ConfigLED0();
 
--- template_src/gpio_template.c
+++ template_src/gpio_template.c
@@ -11,10 +11,10 @@ void ConfigLED0()
 {
 	EALLOW;
 	CLEARLED0();
-	GpioCtrlRegs.GPAMUX2.bit.GPIO29 = 0;         // GPIO
-	GpioCtrlRegs.GPADIR.bit.GPIO29 = 1;          // output
-	GpioCtrlRegs.GPAQSEL2.bit.GPIO29 = 0;        //Synch to SYSCLKOUT only
-	GpioCtrlRegs.GPAPUD.bit.GPIO29 = 1; 		//disable pull up
+	GpioCtrlRegs.GPAMUX2.bit.GPIO22 = 0;         // GPIO
+	GpioCtrlRegs.GPADIR.bit.GPIO22 = 1;          // output
+	GpioCtrlRegs.GPAQSEL2.bit.GPIO22 = 0;        //Synch to SYSCLKOUT only
+	GpioCtrlRegs.GPAPUD.bit.GPIO22 = 1; 		//disable pull up
 	EDIS;
 }
 
@@ -22,13 +22,14 @@ void ConfigLED1()
 {
 	EALLOW;
 	CLEARLED1();
-	GpioCtrlRegs.GPAMUX1.bit.GPIO9 = 0;         // GPIO
-	GpioCtrlRegs.GPADIR.bit.GPIO9 = 1;          // output
-	GpioCtrlRegs.GPAQSEL1.bit.GPIO9 = 0;        //Synch to SYSCLKOUT only
-	GpioCtrlRegs.GPAPUD.bit.GPIO9 = 1; 		//disable pull up
+	GpioCtrlRegs.GPAMUX2.bit.GPIO23 = 0;         // GPIO
+	GpioCtrlRegs.GPADIR.bit.GPIO23 = 1;          // output
+	GpioCtrlRegs.GPAQSEL2.bit.GPIO23 = 0;        //Synch to SYSCLKOUT only
+	GpioCtrlRegs.GPAPUD.bit.GPIO23 = 1; 		//disable pull up
 	EDIS;
 }
 
+/*
 void Config12V()
 {
 	EALLOW;
@@ -39,14 +40,15 @@ void Config12V()
 	GpioCtrlRegs.GPAPUD.bit.GPIO20 = 1; 		//disable pull up
 	EDIS;
 }
+*/
 
 
 void ConfigGPButton()
 {
 	EALLOW;
-	GpioCtrlRegs.GPAMUX2.bit.GPIO21 = 0;         // GPIO
-	GpioCtrlRegs.GPADIR.bit.GPIO21 = 0;          // input
-	GpioCtrlRegs.GPAQSEL2.bit.GPIO21 = 0;        //Synch to SYSCLKOUT only
-	GpioCtrlRegs.GPAPUD.bit.GPIO21 = 0; 		//enable pull up
+	GpioCtrlRegs.GPAMUX1.bit.GPIO11 = 0;         // GPIO
+	GpioCtrlRegs.GPADIR.bit.GPIO11 = 0;          // input
+	GpioCtrlRegs.GPAQSEL1.bit.GPIO11 = 0;        //Synch to SYSCLKOUT only
+	GpioCtrlRegs.GPAPUD.bit.GPIO11 = 0; 		//enable pull up
 	EDIS;
 }
